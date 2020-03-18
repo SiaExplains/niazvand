2a954099291cb9b9e9402c2bb9aae01f851887c5
sia.qnbr@gmail.com <sia.qnbr@gmail.com>
2020-03-18 14:29:11 +0100

    a simple restructure

---
+++ client/src/components/main-app.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+import './main-app.module.css';
+
+class MainApp extends Component {
+    render() {
+        return (
+            <div className='App'>
+                <header className='App-header'>
+                    <p>
+                        This is a change! Edit <code>src/App.js</code> and save
+                        to reload.
+                    </p>
+                    <a
+                        className='App-link'
+                        href='https://reactjs.org'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default MainApp;
